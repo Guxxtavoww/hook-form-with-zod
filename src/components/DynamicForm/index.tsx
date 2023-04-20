345da7c8a80e20fcb35f5ad2c086bad067fa622c
Gustavo <gustavoaugustocar@hotmail.com>
2023-04-20 15:18:58 -0300

    limpando código

--- src/components/DynamicForm/index.tsx
+++ src/components/DynamicForm/index.tsx
@@ -1,4 +1,5 @@
-import { useForm } from 'react-hook-form';
+import { useMemo } from 'react';
+import { useForm, FormProvider, Path } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { iDynamicFormProps, GenericType } from './types';
@@ -8,37 +9,39 @@ function DynamicForm<T extends GenericType>(
 ): JSX.Element {
   const { schema, inputs, handleSubmit, children } = props;
 
+  const form = useForm<T>({
+    resolver: zodResolver(schema),
+  });
+
   const {
     formState,
     register,
     handleSubmit: onSubmit,
-  } = useForm({
-    resolver: zodResolver(schema),
-  });
+  } = useMemo(() => form, [form]);
 
   return (
-    <form onSubmit={onSubmit((data) => handleSubmit(data as T))}>
-      {!!inputs
-        ? inputs.map((input, index) => (
-            <div key={index}>
-              {input.label ? (
-                <label htmlFor={input.name.toString()}>{input.label}</label>
-              ) : null}
-              <input
-                type="text"
-                id={String(input.name)}
-                {...register(input.name.toString())}
-              />
-              {formState.errors[input.name]?.message ? (
-                <span>
-                  {String(formState.errors[input.name]?.message)}
-                </span>
-              ) : null}
-            </div>
-          ))
-        : children}
+    <FormProvider {...form}>
+      <form onSubmit={onSubmit((data) => handleSubmit(data as T))}>
+        {!!inputs
+          ? inputs.map((input, index) => (
+              <div key={index}>
+                {input.label ? (
+                  <label htmlFor={input.name.toString()}>{input.label}</label>
+                ) : null}
+                <input
+                  type="text"
+                  id={String(input.name)}
+                  {...register(input.name.toString() as Path<T>)}
+                />
+                {formState.errors[input.name]?.message ? (
+                  <span>{String(formState.errors[input.name]?.message)}</span>
+                ) : null}
+              </div>
+            ))
+          : children}
         <button type="submit">Teste</button>
-    </form>
+      </form>
+    </FormProvider>
   );
 }
 
